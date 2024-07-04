f0908d8bfe967db73291e84e4d6422bab0ebec05
urly3 <u@rl.yyy>
2024-07-04 18:54:15 +0100

    user class. to decide if i want to put the calculation logic in here or in the match or somewhere else.. utils extenstions? would keep the class clean but i mean is it that big of a deal really..

--- Models/Internal/User.cs
+++ Models/Internal/User.cs
@@ -2,5 +2,22 @@ namespace OMA.Models.Internal;
 
 class User
 {
-    
+    public int Id { get; set; }
+    public string AvatarUrl { get; set; } = default!;
+    public string  Username { get; set; } = default!;
+    public string CountryName { get; set; } = default!;
+    public string CountryCode { get; set; } = default!;
+
+    public Team Team { get; set; } = Team.None;
+
+    public float MatchCostTeam { get; set; }
+    public float MatchCostTotal { get; set; }
+
+    public long AverageScore { get; set; }
+    public long HighestScore { get; set; }
+    public long LowestScore { get; set; }
+
+    public float AverageAccuracy { get; set; }
+    public float HighestAccuracy{ get; set; }
+    public float LowestAccuracy { get; set; }
 }
