e71388aa5e6a6ff83d899f47074f55b8476a0409
urly3 <u@rl.yyy>
2024-08-09 19:46:54 +0100

    tracked files.

---
+++ Migrations/20240808233343_Init.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OMA.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aliases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Hash = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aliases", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lobbies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LobbyId = table.Column<long>(type: "INTEGER", nullable: false),
+                    BestOf = table.Column<int>(type: "INTEGER", nullable: false),
+                    Warmups = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lobbies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AliasLobby",
+                columns: table => new
+                {
+                    AliasesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    LobbiesId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AliasLobby", x => new { x.AliasesId, x.LobbiesId });
+                    table.ForeignKey(
+                        name: "FK_AliasLobby_Aliases_AliasesId",
+                        column: x => x.AliasesId,
+                        principalTable: "Aliases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AliasLobby_Lobbies_LobbiesId",
+                        column: x => x.LobbiesId,
+                        principalTable: "Lobbies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AliasLobby_LobbiesId",
+                table: "AliasLobby",
+                column: "LobbiesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AliasLobby");
+
+            migrationBuilder.DropTable(
+                name: "Aliases");
+
+            migrationBuilder.DropTable(
+                name: "Lobbies");
+        }
+    }
+}
