feec4dab45d8d7272ee84044927c762140dc35de
urly3 <u@rl.yyy>
2024-08-13 12:40:22 +0100

    wip: error codes maybe make some kind of union type to replace the exceptions. otherwise, custom exceptions.

---
+++ Services/OMAStatus.cs
@@ -0,0 +1,24 @@
+namespace OMA.Services;
+
+enum OMAStatus
+{
+    ActionFailed,
+
+    AliasCrated,
+    AliasDoesNotExist,
+    AliasExists,
+    AliasLocked,
+    AliasUnlocked,
+    InvalidPassword,
+
+    LobbyAdded,
+    LobbyRemoved,
+    LobbyDoesNotExist,
+
+    MatchBadResponse,
+    MatchUrlInvalid,
+    MatchTypeInvalid,
+    MatchFailedToDeserialise,
+    MatchHasNoUsers,
+    MatchHasNoEvents,
+}
