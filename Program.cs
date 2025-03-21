385235f9829c0ada08599e2a08764b4eb5e374bb
urly3 <u@rl.yyy>
2025-03-21 17:44:31 +0000

    no https

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 using OMA.Core.Data;
 
-var builder = WebApplication.CreateSlimBuilder(new WebApplicationOptions
-{
-    //WebRootPath = Directory.GetCurrentDirectory() + "\\wwwroot",
-});
+var builder = WebApplication.CreateSlimBuilder();
 
 builder.WebHost.UseIIS();
 builder.WebHost.UseIISIntegration();
@@ -15,15 +12,6 @@ builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.MapControllers();
 
